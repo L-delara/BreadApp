e2c465946484647a96fd5fb0d30856bad80850ca
L-delara <ligia.delara15@gmail.com>
2023-05-02 20:55:11 -0400

    linked new-home pages

--- views/new.jsx
+++ views/new.jsx
@@ -4,7 +4,27 @@ const Default = require("./layouts/default");
 function New() {
   return (
     <Default>
-      <h2>New Page</h2>
+      <h2>Add a new bread</h2>
+      <div className="newButton">
+        <a href="/breads/">
+          <button>Home</button>
+        </a>
+      </div>
+      <form action="/breads" method="POST">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" required />
+        <label htmlFor="image">Image</label>
+        <input type="text" name="image" id="image" />
+        <label htmlFor="hasGluten">Has Gluten?</label>
+        <input
+          type="checkbox"
+          name="hasGluten"
+          id="hasGluten"
+          defaultChecked
+        />
+        <br />
+        <input type="submit" />
+      </form>
     </Default>
   );
 }
