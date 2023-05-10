fffa789a14db819f2ef884f1ad3c47212a95ba25
L-delara <ligia.delara15@gmail.com>
2023-05-09 20:49:17 -0400

    connect to database

--- controllers/bread.js
+++ controllers/bread.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 const Bread = require("../models/bread");
 
 //GET all the bread
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const bread = await Bread.find();
   res.render("index", {
-    breads: Bread,
+    breads: bread,
   });
 });
 
@@ -13,11 +14,11 @@ router.get("/new", (req, res) => {
   res.render("new");
 });
 
-router.get("/:index", (req, res) => {
-  const { index } = req.params;
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const bread = await Bread.findById(id);
   res.render("show", {
-    bread: Bread[index],
-    index,
+    bread,
   });
 });
 
@@ -29,17 +30,16 @@ router.get("/:index/edit", (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {
-  if (!req.body.image)
-    req.body.image =
-      "https://img.freepik.com/premium-vector/cute-funny-bread-toast-character_92289-2249.jpg";
+router.post("/", async (req, res) => {
+  if (!req.body.image) req.body.image = undefined;
+
   if (req.body.hasGluten === "on") {
     req.body.hasGluten = true;
   } else {
     req.body.hasGluten = false;
   }
-  Bread.push(req.body);
-  res.redirect("/breads");
+  await Bread.create(req.body);
+  res.status(303).redirect("/breads");
 });
 
 router.delete("/:index", (req, res) => {
@@ -50,9 +50,7 @@ router.delete("/:index", (req, res) => {
 
 router.put("/:index", (req, res) => {
   const { index } = req.params;
-  if (!req.body.image)
-    req.body.image =
-      "https://img.freepik.com/premium-vector/cute-funny-bread-toast-character_92289-2249.jpg";
+  // if (!req.body.image)  req.body.image = "https://img.freepik.com/premium-vector/cute-funny-bread-toast-character_92289-2249.jpg";
   if (req.body.hasGluten === "on") {
     req.body.hasGluten = true;
   } else {
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const breadController = require("./controllers/bread");
 
@@ -12,6 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+
+// db connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error(err));
 
 //routes
 app.use("/breads", breadController);
